2d81679f28cbd79caaa7723aa1cad937c61f4d35
m1172 <muhshuh2000@gmail.com>
2021-11-16 23:12:10 +0100

    edited

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,14 +1,15 @@
 const MyMessage = ({ message }) => {
-  if (message?.attachments?.length > 0) {
+  if (message.attachments && message.attachments.length > 0) {
     return (
       <img
-        src='{message.attachments[0].file}'
+        src={message.attachments[0].file}
         alt='message-attachment'
         className='message-image'
         style={{ float: 'right' }}
       />
     );
   }
+
   return (
     <div
       className='message'
