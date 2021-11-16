e7bbce5c0dd386321ea5e57ef205e5cc9d75fb5a
m1172 <muhshuh2000@gmail.com>
2021-11-16 10:43:27 +0100

    edited

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,5 +1,16 @@
-const TheirMessage = () => {
-  return <div>TheirMesaage</div>;
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username != message.sender.username;
+  return (
+    <div className='message-row'>
+      {isFirstMessageByUser(
+        <div
+          className='message-avatar'
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default TheirMessage;
